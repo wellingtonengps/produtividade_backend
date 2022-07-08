d7ffc26f428c21a64f4e01c3577a47c6a57a39b0
Wellington Pereira <43380060+wellingtonengps@users.noreply.github.com>
2022-07-08 10:03:11 -0300

    fix: migration with docker

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
-FROM node:16.15.0
+FROM node:16.15.0-alpine
 WORKDIR /usr/app
 COPY package*.json ./
 RUN npm install
 COPY . .
-EXPOSE 3333
+ENV PORT=$PORT
+EXPOSE $PORT
 CMD ["npm","run","dev"]
---
+++ database/migrations/1657283365523_vehicles.ts
@@ -0,0 +1,24 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'vehicles'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name')
+      table.string('description')
+      table.string('plate')
+      table.boolean('isFavorite')
+      table.integer('year')
+      table.string('color')
+      table.integer('price')
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
